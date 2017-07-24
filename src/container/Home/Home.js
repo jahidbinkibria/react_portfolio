0ea5dee1fd922ec3bcb5c157ebe1ac4f510497f5
jahid <jk311857@gmail.com>
2017-07-25 01:36:43 +0600

    using react-waypoint

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,6 +1,7 @@
 import React ,{Component,PropTypes} from "react";
 import {Container} from '../../theme/grid';
 import {Image,HeroImage,RevealP} from './Home.style';
+import Wheninview from "../../component/wheninview/wheninview";
 
 
 
@@ -15,9 +16,13 @@ export default  class Home extends Component{
                     <h2>Front-end Developer</h2>
                 </HeroImage>
                 {/* <Image src={require("../asset/hero.gif")} alt="Hero Image"/> */}
-                <RevealP>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione rem nobis dolorum eius autem alias optio veniam totam voluptatibus a, ipsa blanditiis quisquam dolores molestias, similique laboriosam iste suscipit magnam maxime quia ut. Unde quam eaque magni doloribus cum harum adipisci minus ipsam, maxime. Facere itaque, similique esse repudiandae cum corrupti ullam ipsam! Dolor necessitatibus earum atque aut, hic molestias tempora! Placeat saepe hic, nihil, libero nobis fugit odit nesciunt cumque nisi temporibus, possimus iusto vel architecto. Dolor, illo, nesciunt?
-                </RevealP>
+               <Wheninview>
+                   {({isInView}) => 
+                    <RevealP hide={!isInView}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione rem nobis dolorum eius autem alias optio veniam totam voluptatibus a, ipsa blanditiis quisquam dolores molestias, similique laboriosam iste suscipit magnam maxime quia ut. Unde quam eaque magni doloribus cum harum adipisci minus ipsam, maxime. Facere itaque, similique esse repudiandae cum corrupti ullam ipsam! Dolor necessitatibus earum atque aut, hic molestias tempora!
+                    </RevealP>
+                   } 
+               </Wheninview>
             </Container>
         )
     }
